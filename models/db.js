7f2c073b3d0b3de7c06b8810ca119f13fcd510f9
manojm01 <manojmetgud035@gmail.com>
2022-02-10 23:37:24 +0530

    cloud connected

--- models/db.js
+++ models/db.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/EmployeeDB', {useNewUrlParser:true} , (err)=>{
+mongoose.connect('mongodb+srv://manoj:manoj@cluster0.eku64.mongodb.net/EmployeeDB?retryWrites=true&w=majority', {useNewUrlParser:true} , (err)=>{
 
     if(!err){console.log('MongoDB Connection Succeeded');}
     else {console.log('Error in DB Connection: '+ err);}
 
 });
+// mongoose.connect('mongodb://localhost:27017/EmployeeDB', {useNewUrlParser:true} , (err)=>{
+
+//     if(!err){console.log('MongoDB Connection Succeeded');}
+//     else {console.log('Error in DB Connection: '+ err);}
+
+// });
 
 require('./employee.models');
